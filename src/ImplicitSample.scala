72bbcc333a3564a8a17f84bdbe0add8ae459a2d6
tattyamm <entatsu.appli+github@gmail.com>
2012-08-05 21:04:59 +0900

    暗黙の型変換と、暗黙の引数について書いた

--- src/ImplicitSample.scala
+++ src/ImplicitSample.scala
@@ -1,30 +1,14 @@
 /**
  * implicitまわりがよくわからなかったのでサンプル作成。
  * 参考  コップ本
- * http://twitter.github.com/scala_school/advanced-types.html
- * http://d.hatena.ne.jp/unageanu/20080531/1212232674
- * http://tech.cm55.com/wiki/scala/implicit
+ *       http://twitter.github.com/scala_school/advanced-types.html
+ *       http://d.hatena.ne.jp/unageanu/20080531/1212232674
+ *       http://tech.cm55.com/wiki/scala/implicit
+ *
+ *
  */
 
 object ImplicitSample {
-
-  def main(args: Array[String]) {
-
-    //暗黙の引数 (implicit parameters)
-    methodWithImplicitArg1
-    methodWithImplicitArg1("hoge")
-
-    //型で判断して暗黙的に供給してくれる
-    //  valに限らず、関数でもオブジェクトでもかまわない
-    implicit val intValue = 10
-    implicit val longValue = 20L
-    methodWithImplicitArg2
-    methodWithImplicitArg2(3, 1L)
-
-    //暗黙の型変換
-    println(math.max("123", 111))
-
-  }
 
   //暗黙の引数 (implicit parameters)
   def methodWithImplicitArg1(implicit s: String = "Sampple Text") {
@@ -35,7 +19,40 @@ object ImplicitSample {
     println("methodWithImplicitArg called:" + a * b)
   }
 
+  def methodWithImplicitArg3(implicit s: SampleClass) {
+    println("methodWithImplicitArg called:" + s)
+  }
+  implicit val toBeImplicitArg = new SampleClass(1);
+  class SampleClass(value: Int) {
+    override def toString = { "Sample:" + value }
+  }
+
   //暗黙の型変換
   implicit def strToInt(x: String) = x.toInt
+
+  def main(args: Array[String]) {
+
+    //暗黙の引数 (implicit parameters)
+    //  引数を省略する事が可能になる。
+    methodWithImplicitArg1
+    methodWithImplicitArg1()
+    methodWithImplicitArg1("hoge")
+
+    //  型で判断して暗黙的に供給してくれる
+    implicit val intValue = 10
+    implicit val longValue = 20L
+    methodWithImplicitArg2
+    methodWithImplicitArg2(3, 1L)
+
+    //  valに限らず、関数でもオブジェクトでもかまわない
+    methodWithImplicitArg3
+
+
+
+    //暗黙の型変換 (implicit conversion)
+    //  乱用すると、訳が分からなくなる。
+    println(math.max("123", 111))
+
+  }
 
 }
